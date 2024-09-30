5ed6f5e7aa4dbcde7d9fb1b55b6cc68cad89cb28
vidit <viditsharma818@gmail.com>
2024-09-30 14:02:09 +0530

    completed

--- program1.js
+++ program1.js
@@ -2,10 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+  if (s.length % 2 !== 0) return false;
+
+  const stack = [];
+  const bracketMap = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+
+  for (let char of s) {
+    if (!bracketMap[char]) {
+      stack.push(char);
+    } else if (stack.pop() !== bracketMap[char]) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 };
 
 module.exports = { isValid };
-
-
